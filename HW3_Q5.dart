2455f2e64bbb3d60c29b3c6e48b6c6bf1d85af7f
ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>
2023-05-08 02:33:48 +0300

    nyt
    Signed-off-by: ALWALEED-KHUSHAIM <doode711.cr7@gmail.com>

---
+++ HW3_Q5.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  int a = 10;
+  int b = 4;
+  var x="/";
+  if (x == "+") {
+    print(a + b);
+  }
+  else if (x == "-") {
+    print(a - b);
+  }else if (x == "/") {
+    print(a / b);
+  }else if (x == "*") {
+    print(a * b);
+  }
+}
