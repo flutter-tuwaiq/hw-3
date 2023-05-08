030093a57ef4fb0a7735d3140ff1d63ee329ca55
ZiyadALharbi <ziyad.swengineer@gmail.com>
2023-05-08 08:33:52 +0300

    Update Q11.dart

--- bin/Q11.dart
+++ bin/Q11.dart
@@ -4,7 +4,7 @@ void main() {
     'phone': "0544044630",
   };
       
-  Iterable<String> keysWithLength4 = contact.keys.where((key) => key.length == 4);
+  var  keysWithLength4 = contact.keys.where((key) => key.length == 4);
 
   print('Keys with length 4: $keysWithLength4');
 }
